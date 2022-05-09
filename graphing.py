bc59ad656f08725da6d07c737fefea96895c6288
Sina <sinagolchi@gmail.com>
2022-05-09 18:25:55 -0400

    Add some features :\

--- graphing.py
+++ graphing.py
@@ -47,14 +47,14 @@ class data_set():
 
         self.processed_sets = [range_set(s) for s in self.sub_frames]
 
-    def plot_all(self):
+    def plot_all(self,legend_style='outside'):
         handles = []
-        self.colors = (c for c in ['#e6194b', '#3cb44b', '#f58231', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6'])
+        self.colors = (c for c in ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'])
         self.markers = (m for m in ['v','s','o','d'])
-        self.offset = (t for t in [-1,0,1,1,1.5])
-        self.num_keys1 = (k for k in range(1,10))
-        self.num_keys2 = (k for k in range(10,25))
-        self.check_keys = (k for k in range(25,50))
+        self.offset = (t for t in [-2.5,-2,-1.5,-1,-0.5,0,0.5,1,1.5,2,2.5])
+        self.num_keys1 = (k for k in range(1,50))
+        self.num_keys2 = (k for k in range(50,100))
+        self.check_keys = (k for k in range(100,150))
         for set in self.processed_sets:
             with st.expander(label=set.DWTP + ' ' + set.Study + ': propertise'):
                 col1, col2 , col3 , col4 = st.columns(4)
@@ -70,7 +70,12 @@ class data_set():
                     t_alpha = st.number_input(label='Trend-line Transparency',value=1.0,min_value=0.2,max_value=1.0,step=0.1,key=next(self.num_keys2))
 
             set.plot(handle=handles,color=color,marker=marker,offset=offset,show_trend_line=trend,alpha=alpha,trend_line_alpha=t_alpha)
-        plt.legend(handles=handles)
+        if legend_style == 'outside':
+            plt.legend(handles=handles,fontsize=9,loc='center right',bbox_to_anchor=[1.6,0.9])
+        elif legend_style == 'inside':
+            plt.legend(handles=handles,fontsize=9)
+        else:
+            pass
 
 #%%
 
@@ -83,7 +88,7 @@ class data_set():
 # plt.ylabel('Removal efficiency')
 # plt.gcf().set_size_inches([6,4])
 # plt.show()
-
+st.set_page_config(layout='wide',page_title='Graphing tool')
 st.title('Graphing for particle removal')
 st.caption('Developed by Sina Golchi for NSERC Chair in Water Wreatment')
 
@@ -92,8 +97,10 @@ with st.sidebar:
 
 try:
     dft = data_set(csv)
-    dft.plot_all()
+
     with st.sidebar:
+        st.markdown('''___''')
+        st.caption('Axis options')
         col1, col2  = st.columns(2)
         with col1:
             xmin = st.number_input(label='x min',value=-5)
@@ -103,6 +110,12 @@ try:
             ymax = st.number_input(label='y max', value=110)
         xlog = st.checkbox(label='Logarithmic x axis')
         ylog = st.checkbox(label='Logarithmic y axis')
+
+        st.markdown('''___''')
+        st.caption('Legend option')
+        L_type = st.selectbox(label='Legend type',options=['outside','inside','no legend'])
+
+    dft.plot_all(legend_style=L_type)
 
     if xlog:
         plt.xscale('log')
@@ -114,6 +127,8 @@ try:
 
     plt.xlabel('Particle range [$\mu m$]')
     plt.ylabel('Removal efficiency [%]')
+    plt.grid(axis='y',alpha=0.4)
+    plt.gcf().set_size_inches(7,4)
     st.pyplot(plt.gcf())
 except Exception as ex:
     print(ex)
